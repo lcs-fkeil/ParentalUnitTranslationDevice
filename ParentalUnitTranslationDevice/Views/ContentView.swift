810de95093a44ca6aa02f39f82bc01cffc0251db
Franka Keil <fkeil@lcs.on.ca>
2021-12-11 10:23:12 -0500

    Added a static layout of my app.

--- ParentalUnitTranslationDevice/Views/ContentView.swift
+++ ParentalUnitTranslationDevice/Views/ContentView.swift
@@ -7,15 +7,43 @@
 
 import SwiftUI
 
+// MARK: Stored properties
+var shortForm: String = ""
+
+
+// MARK: Computed properties
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ScrollView {
+            HStack {
+                VStack (alignment: .leading){
+                    Text("Short form:")
+                        .font(.title)
+                        .underline()
+                    
+                    // user input
+                    Text(shortForm)
+                        .bold()
+                        .padding(10)
+                    
+                    
+                    Text("Translation:")
+                        .font(.title)
+                        .underline()
+                    
+                }
+                Spacer()
+            }
+        }
+        .navigationTitle("Short form translation")
+        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        NavigationView{
+            ContentView()
+        }
     }
 }
